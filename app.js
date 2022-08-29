b9a80d440ee1f934ea024994077537ab9e6ca364
LMV-Dev <develop@locomotionview.com>
2022-08-29 13:47:50 +0900

    Add cors

--- app.js
+++ app.js
@@ -79,11 +79,11 @@ if (prodChecker) {
             credentials: true,
         }),
     )
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*')
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-        next()
-    })
+    // app.use((req, res, next) => {
+    //     res.header('Access-Control-Allow-Origin', '*')
+    //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    //     next()
+    // })
 }
 
 /* View Engine */
