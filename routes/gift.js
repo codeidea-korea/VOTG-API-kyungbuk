6006380acb9229cf622ef7c5c43bd38e9b5a13ec
LMV-Dev <develop@locomotionview.com>
2022-10-17 16:43:48 +0900

    Edit log

--- routes/gift.js
+++ routes/gift.js
@@ -44,33 +44,53 @@ router.post('/issued', async (req, res) => {
         // const id = req.params.id
 
         const curDate = moment().format('YYYYMMDD')
-        const reqData = {
-            // ACTION: 'CI102_ISSUECPN_WITHPAY',
-            SITE_ID: '10001171',
-            COOPER_ID: 'SC0292',
-            COOPER_PW: 'heen78@!',
-            // NO_REQ: giftiReqNo,
-            NO_REQ: '44846', // 개발서버 테스트용
-            // NO_REQ: '222983', // 실서버 츄파춥스
-            COOPER_ORDER: `${moment().format('YYYYMMDDHHmmssSSS')}`,
-            ISSUE_COUNT: '1',
-            CALL_CTN: '18991294',
-            SENDER: 'VOTG-DEVELOP',
-            RCV_CTN: phone,
-            // RECEIVER: `${user.name.toString()}`,
-            SEND_MSG: '뷰즈온더고 리워드 기프티콘입니다.',
-            VALID_START: `${curDate}`,
-            VALID_END: `${moment(curDate).add(1, 'y').format('YYYYMMDD')}`,
-            PAY_ID: 'none',
-            BOOKING_NO: 'none',
-            SITE_URL: 'none',
-        }
+        // const reqData = {
+        //     // ACTION: 'CI102_ISSUECPN_WITHPAY',
+        //     SITE_ID: '10001171',
+        //     COOPER_ID: 'SC0292',
+        //     COOPER_PW: 'heen78@!',
+        //     // NO_REQ: giftiReqNo,
+        //     NO_REQ: '44846', // 개발서버 테스트용
+        //     // NO_REQ: '222983', // 실서버 츄파춥스
+        //     COOPER_ORDER: `${moment().format('YYYYMMDDHHmmssSSS')}`,
+        //     ISSUE_COUNT: '1',
+        //     CALL_CTN: '18991294',
+        //     SENDER: 'VOTG-DEVELOP',
+        //     RCV_CTN: phone,
+        //     // RECEIVER: `${user.name.toString()}`,
+        //     SEND_MSG: '뷰즈온더고 리워드 기프티콘입니다.',
+        //     VALID_START: `${curDate}`,
+        //     VALID_END: `${moment(curDate).add(1, 'y').format('YYYYMMDD')}`,
+        //     PAY_ID: 'none',
+        //     BOOKING_NO: 'none',
+        //     SITE_URL: 'none',
+        // }
 
         const sendGiftcon = await axios({
             url: 'https://atomtest.donutbook.co.kr:14076/b2ccoupon/b2cservice.asox',
             method: 'post', // POST method
             headers: { 'Content-Type': 'application/json' }, // "Content-Type": "application/json"
-            data: qs.stringify(reqData),
+            data: {
+                // ACTION: 'CI102_ISSUECPN_WITHPAY',
+                SITE_ID: '10001171',
+                COOPER_ID: 'SC0292',
+                COOPER_PW: 'heen78@!',
+                // NO_REQ: giftiReqNo,
+                NO_REQ: '44846', // 개발서버 테스트용
+                // NO_REQ: '222983', // 실서버 츄파춥스
+                COOPER_ORDER: `${moment().format('YYYYMMDDHHmmssSSS')}`,
+                ISSUE_COUNT: '1',
+                CALL_CTN: '18991294',
+                SENDER: 'VOTG-DEVELOP',
+                RCV_CTN: phone,
+                // RECEIVER: `${user.name.toString()}`,
+                SEND_MSG: '뷰즈온더고 리워드 기프티콘입니다.',
+                VALID_START: `${curDate}`,
+                VALID_END: `${moment(curDate).add(1, 'y').format('YYYYMMDD')}`,
+                PAY_ID: 'none',
+                BOOKING_NO: 'none',
+                SITE_URL: 'none',
+            },
         })
 
         console.log('sendGiftcon', sendGiftcon)
@@ -83,7 +103,7 @@ router.post('/issued', async (req, res) => {
             payload: sendGiftcon,
         })
     } catch (error) {
-        debug.error('sendGiftcon', error)
+        debug.error('sendGiftcon', error.response)
         res.status(400).json({ result: '0', error: error })
     }
 })
@@ -123,9 +143,7 @@ router.post('/issued/public', async (req, res) => {
                 SITE_ID: '10002296',
                 COOPER_ID: 'SC1459',
                 COOPER_PW: 'cwnf98@@',
-                // NO_REQ: giftiReqNo,
                 NO_REQ: '203449', // 개발서버 테스트용
-                // NO_REQ: '222983', // 실서버 츄파춥스
                 COOPER_ORDER: `${moment().format('YYYYMMDDHHmmssSSS')}`,
                 ISSUE_COUNT: '1',
                 CALL_CTN: '18991294',
@@ -151,7 +169,7 @@ router.post('/issued/public', async (req, res) => {
             payload: sendGiftcon,
         })
     } catch (error) {
-        debug.error('sendGiftcon', error)
+        debug.error('sendGiftcon', error.response)
         res.status(400).json({ result: '0', error: error })
     }
 })
