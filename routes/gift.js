9a32d555bafde3e47a2e62fa6b31392f85492466
LMV-Dev <develop@locomotionview.com>
2022-10-17 17:03:26 +0900

    Add

--- routes/gift.js
+++ routes/gift.js
@@ -44,46 +44,24 @@ router.post('/issued', async (req, res) => {
         // const id = req.params.id
 
         const curDate = moment().format('YYYYMMDD')
-        // const reqData = {
-        //     // ACTION: 'CI102_ISSUECPN_WITHPAY',
-        //     SITE_ID: '10001171',
-        //     COOPER_ID: 'SC0292',
-        //     COOPER_PW: 'heen78@!',
-        //     // NO_REQ: giftiReqNo,
-        //     NO_REQ: '44846', // 개발서버 테스트용
-        //     // NO_REQ: '222983', // 실서버 츄파춥스
-        //     COOPER_ORDER: `${moment().format('YYYYMMDDHHmmssSSS')}`,
-        //     ISSUE_COUNT: '1',
-        //     CALL_CTN: '18991294',
-        //     SENDER: 'VOTG-DEVELOP',
-        //     RCV_CTN: phone,
-        //     // RECEIVER: `${user.name.toString()}`,
-        //     SEND_MSG: '뷰즈온더고 리워드 기프티콘입니다.',
-        //     VALID_START: `${curDate}`,
-        //     VALID_END: `${moment(curDate).add(1, 'y').format('YYYYMMDD')}`,
-        //     PAY_ID: 'none',
-        //     BOOKING_NO: 'none',
-        //     SITE_URL: 'none',
-        // }
-
+        console.log('phone', phone)
+        console.log('curDate', curDate)
         const sendGiftcon = await axios({
             url: 'https://atomtest.donutbook.co.kr:14076/b2ccoupon/b2cservice.asox',
             method: 'post', // POST method
-            headers: { 'Content-Type': 'application/json' }, // "Content-Type": "application/json"
+            // headers: { 'Content-Type': 'application/json' }, // "Content-Type": "application/json"
             data: {
                 // ACTION: 'CI102_ISSUECPN_WITHPAY',
                 SITE_ID: '10001171',
                 COOPER_ID: 'SC0292',
                 COOPER_PW: 'heen78@!',
-                // NO_REQ: giftiReqNo,
                 NO_REQ: '44846', // 개발서버 테스트용
-                // NO_REQ: '222983', // 실서버 츄파춥스
-                COOPER_ORDER: `${moment().format('YYYYMMDDHHmmssSSS')}`,
+                COOPER_ORDER: `VOTG-${moment().format('YYYYMMDDHHmmssSSS')}`,
                 ISSUE_COUNT: '1',
                 CALL_CTN: '18991294',
                 SENDER: 'VOTG-DEVELOP',
                 RCV_CTN: phone,
-                // RECEIVER: `${user.name.toString()}`,
+                RECEIVER: `user-${phone}`,
                 SEND_MSG: '뷰즈온더고 리워드 기프티콘입니다.',
                 VALID_START: `${curDate}`,
                 VALID_END: `${moment(curDate).add(1, 'y').format('YYYYMMDD')}`,
@@ -108,70 +86,53 @@ router.post('/issued', async (req, res) => {
     }
 })
 router.post('/issued/public', async (req, res) => {
-    try {
-        // const { param_code, param_name } = req.params
-        const { phone } = req.body
-        // const id = req.params.id
+    var data = JSON.stringify({
+        SITE_ID: '10002296',
+        COOPER_ID: 'SC1459',
+        COOPER_PW: 'cwnf98@@',
+        NO_REQ: '203449',
+        COOPER_ORDER: 'VOTG-01',
+        ISSUE_COUNT: '1',
+        CALL_CTN: '18991294',
+        SENDER: 'VOTG',
+        RCV_CTN: '01042152535',
+        RECEIVER: 'user-01042152535',
+        SEND_MSG: '뷰즈온더고 리워드 기프티콘입니다.',
+        VALID_START: '20221017',
+        VALID_END: '59D',
+        PAY_ID: 'none',
+        BOOKING_NO: 'none',
+        SITE_URL: 'none',
+    })
 
-        const curDate = moment().format('YYYYMMDD')
-        // const reqData = {
-        //     SITE_ID: '10002296',
-        //     COOPER_ID: 'SC1459',
-        //     COOPER_PW: 'cwnf98@@',
-        //     // NO_REQ: giftiReqNo,
-        //     NO_REQ: '203449', // 개발서버 테스트용
-        //     // NO_REQ: '222983', // 실서버 츄파춥스
-        //     COOPER_ORDER: `${moment().format('YYYYMMDDHHmmssSSS')}`,
-        //     ISSUE_COUNT: '1',
-        //     CALL_CTN: '18991294',
-        //     SENDER: 'VOTG',
-        //     RCV_CTN: phone,
-        //     // RECEIVER: `${user.name.toString()}`,
-        //     SEND_MSG: '뷰즈온더고 리워드 기프티콘입니다.',
-        //     VALID_START: `${curDate}`,
-        //     VALID_END: `59D`,
-        //     PAY_ID: 'none',
-        //     BOOKING_NO: 'none',
-        //     SITE_URL: 'none',
-        // }
-
-        const sendGiftcon = await axios({
-            url: 'https://atom.donutbook.co.kr/b2ccoupon/b2cservice.aspx',
-            method: 'post', // POST method
-            headers: { 'Content-Type': 'application/json' }, // "Content-Type": "application/json"
-            data: {
-                SITE_ID: '10002296',
-                COOPER_ID: 'SC1459',
-                COOPER_PW: 'cwnf98@@',
-                NO_REQ: '203449', // 개발서버 테스트용
-                COOPER_ORDER: `${moment().format('YYYYMMDDHHmmssSSS')}`,
-                ISSUE_COUNT: '1',
-                CALL_CTN: '18991294',
-                SENDER: 'VOTG',
-                RCV_CTN: phone,
-                RECEIVER: `user-${phone}`,
-                SEND_MSG: '뷰즈온더고 리워드 기프티콘입니다.',
-                VALID_START: `${curDate}`,
-                VALID_END: `59D`,
-                PAY_ID: 'none',
-                BOOKING_NO: 'none',
-                SITE_URL: 'none',
-            },
-        })
-
-        console.log('sendGiftcon', sendGiftcon)
-        debug.axios('sendGiftcon', sendGiftcon)
-
-        return res.status(201).json({
-            isSuccess: true,
-            code: 201,
-            msg: 'ok',
-            payload: sendGiftcon,
-        })
-    } catch (error) {
-        debug.error('sendGiftcon', error.response)
-        res.status(400).json({ result: '0', error: error })
+    var config = {
+        method: 'post',
+        url: 'https://atom.donutbook.co.kr/b2ccoupon/b2cservice.aspx',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: data,
     }
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data))
+            return res.status(201).json({
+                isSuccess: true,
+                code: 201,
+                msg: 'Giftcon successfully issued',
+                payload: response.data,
+            })
+        })
+        .catch(function (error) {
+            console.log(error)
+            return res.status(401).json({
+                isSuccess: false,
+                code: 401,
+                msg: 'failed',
+                payload: error,
+            })
+        })
 })
 
 module.exports = router
