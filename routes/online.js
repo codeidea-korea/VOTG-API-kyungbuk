df906aca3cbe22f08e598f6a066d65992c81af07
JWLK <utxtion@me.com>
2023-02-26 19:50:01 +0900

    sendingPhoneNumber

--- routes/online.js
+++ routes/online.js
@@ -1210,7 +1210,8 @@ router.post('/survey/answer/eachurl', async (req, res) => {
     // console.log(req)
     try {
         const { eachUrl, phoneCode, surveyCode, answerJson, orderCode, productNumber } = req.body
-        // const sendingPhoneNumber = decipher(phoneCode)
+        const sendingPhoneNumber = decipher(phoneCode)
+        console.log('sendingPhoneNumber', sendingPhoneNumber)
         // debug.request('decipher(phoneCode)', sendingPhoneNumber)
 
         const updateEachAnswer = await DB.SurveyAnswersEachUrl.update(
